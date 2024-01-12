ef393de7e2051e880f40392cf1300bad9fde9f25
randradev <randradevald@gmail.com>
2024-01-12 18:32:57 -0300

    Estructura Sidebar

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -6,9 +6,9 @@ const Card = (data) => {
     const context = useContext(ShoppingCartContext)
     
     return(
-        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg m-10 shadow-lg">
+        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg shadow-lg">
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category.name}</span>
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.image} alt={data.data.image} />
                 <button 
                     className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,15 @@
+import'./styles.css'
+
+const ProductDetail = () => {
+
+    return(
+        <aside className='product-detail flex flex-col fixed right-0 rounded-lg bg-orange-500 border-2 border-black bg-gray-200'>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>Detail</h2>
+                <div>x</div>
+            </div>
+        </aside>
+    )
+}
+
+export default ProductDetail
