99cce2366efb3405d58560c833589eb7973d2c32
randradev <randradevald@gmail.com>
2024-01-15 12:46:40 -0300

    Implementación de iconografía

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context"
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
     const context = useContext(ShoppingCartContext)
@@ -101,8 +102,9 @@ const Navbar = () => {
                         Sign In
                     </NavLink>
                 </li>
-                <li>
-                    🛒{context.count}
+                <li className='flex items-center'>
+                    <ShoppingBagIcon className='h-6 w-6 text-black' />
+                    <div>{context.count}</div>
                 </li>
             </ul>
         </nav>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,15 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import'./styles.css'
 
 const ProductDetail = () => {
 
     return(
-        <aside className='product-detail flex flex-col fixed right-0 rounded-lg bg-orange-500 border-2 border-black bg-gray-200'>
+        <aside className='product-detail flex flex-col fixed right-0 rounded-lg bg-white border-2 border-black bg-gray-200'>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div>x</div>
+                <div>
+                    <XMarkIcon className='h-6 w-6 text-black-500'></XMarkIcon>
+                </div>
             </div>
         </aside>
     )
