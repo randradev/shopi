cdd0379f3775714206d1731ac902617b8d93f301
randradev <randradevald@gmail.com>
2024-01-26 15:18:29 -0300

    Skeleton Loader: Estructura

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,9 @@
-import { useContext } from "react"
-import Layout from "../../Components/Layout"
-import Card from "../../Components/Card"
-import ProductDetail from "../../Components/ProductDetail"
-import { ShoppingCartContext } from "../../Context"
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { useContext } from "react";
+import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
+import ProductDetail from "../../Components/ProductDetail";
+import { ShoppingCartContext } from "../../Context";
+import Skeleton from 'react-loading-skeleton';
 
 function Home() {
 
@@ -20,26 +20,29 @@ function Home() {
             className='rounded-lg border border-black w-80 p-2 mb-6 focus:outline-none'
             onChange={(event) => context.setSearchByTitle(event.target.value)}
           />
-          {context.filteredItems?.length > 0 ? (
+          {context.filteredItems ? (
+            // Cards
             <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-screen-lg'>
-              {context.filteredItems?.map((item) => (
+              {context.filteredItems.map((item) => (
                 <Card key={item.id} data={item}/>
               ))}
             </div>
           ) : (
-            <div className='flex items-center justify-center'>
-                <div className='text-center'>
-                    <div className='flex items-center justify-center mb-4'>
-                        <MagnifyingGlassIcon className='h-16 w-16 text-gray-500'/>
-                    </div>
-                    <h2 className='font-medium text-xl'>Sorry</h2>
-                    <p className='text-gray-500'>No matches were found, try again</p>
+            // Skeleton loader
+            <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-screen-lg'>
+              {Array.from({ length: 12 }, (_, index) => (
+                <div key={index} className='bg-gray-200 p-4 rounded-lg'>
+                  <Skeleton height={20} className='mb-2' style={{ width: '75%', display: 'block' }} />
+                  <Skeleton height={20} className='mb-1' style={{ width: '50%', display: 'block' }} />
+                  <Skeleton height={20} style={{ width: '80%', display: 'block' }} />
                 </div>
+              ))}
             </div>
+
           )}
           <ProductDetail />
         </Layout>
-      )
+    )
 }
 
-export default Home
+export default Home;
