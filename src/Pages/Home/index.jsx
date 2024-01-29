15cc0e0b339452d0824de105833d6afb6bdb7d0c
randradev <randradevald@gmail.com>
2024-01-29 16:31:37 -0300

    Filtro: No Results Message

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 import ProductDetail from "../../Components/ProductDetail";
@@ -7,6 +7,8 @@ import { ShoppingCartContext } from "../../Context";
 function Home() {
 
     const context = useContext(ShoppingCartContext)
+    
+    const [noResultsMessage, setNoResultsMessage] = useState(null)
 
     return (
         <Layout>
@@ -16,16 +18,36 @@ function Home() {
           <input
             type='text'
             placeholder='Search a product'
-            className='rounded-lg border border-black w-80 p-2 mb-6 focus:outline-none'
-            onChange={(event) => context.setSearchByTitle(event.target.value)}
+            className='rounded-lg border border-black w-80 p-2 mb-10 focus:outline-none'
+            onChange={(event) => {
+              const searchTerm = event.target.value;
+              context.setSearchByTitle(searchTerm);
+
+              // Verificar si hay coincidencias y actualizar el mensaje
+              const hasResults = context.filteredItems && context.filteredItems.length > 0;
+              console.log("searchTerm:", searchTerm);
+              console.log("hasResults:", hasResults);
+              setNoResultsMessage(hasResults || searchTerm.trim() === '' ? null : 'No se encontraron coincidencias.');
+            }}
           />
+
           {context.filteredItems ? (
-            // Cards
-            <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-screen-lg'>
-              {context.filteredItems.map((item) => (
-                <Card key={item.id} data={item}/>
-              ))}
-            </div>
+            context.filteredItems.length > 0 ? (
+              // Cards
+              <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-screen-lg'>
+                {context.filteredItems.map((item) => (
+                  <Card key={item.id} data={item}/>
+                ))}
+              </div>
+            ) : (
+              // Mensaje de no hay coincidencias
+              <div className='flex items-center justify-center'>
+                <div className='text-center'>
+                  <h1 className='font-medium text-xl mt-6'>Ups!</h1>
+                  <p className='font-light text-sm'>{noResultsMessage}</p>
+                </div>
+              </div>
+            )
           ) : (
             // Skeleton loader
             <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-screen-lg'>
